790066ad114c0ab06612b930f0ba3421cbcdd283
GoodGuyGarry <zuma222_hazem@hotmail.com>
2019-03-16 20:20:46 +0200

    Add user /table for user/edit user/donation form

---
+++ pages/donation.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html lang="en">
+ 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="../assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/libs/css/style.css">
+    <link rel="stylesheet" href="../assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" href="../assets/vendor/datepicker/tempusdominus-bootstrap-4.css" />
+    <link rel="stylesheet" href="../assets/vendor/inputmask/css/inputmask.css" />
+    
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- main wrapper -->
+    <!-- ============================================================== -->
+    <div class="dashboard-main-wrapper">
+        <!-- ============================================================== -->
+        <!-- navbar -->
+        <!-- ============================================================== -->
+        <?php
+include '../dbheader.html';
+
+?>
+        <!-- ============================================================== -->
+        <!-- end navbar -->
+        <!-- ============================================================== -->
+        <?php
+include '../dashboard.html';
+
+?>
+<div class="dashboard-wrapper">
+            <div class="container-fluid dashboard-content">
+                <div class="row">
+                    <div class="col-xl-10">
+                        <!-- ============================================================== -->
+                        <!-- pageheader  -->
+                        <!-- ============================================================== -->
+                        <div class="row">
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                <div class="page-header" id="top">
+                                    <h2 class="pageheader-title">Donation Form </h2>
+                                    <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
+                                    <div class="page-breadcrumb">
+                                        <nav aria-label="breadcrumb">
+                                            <ol class="breadcrumb">
+                                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
+                                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Forms</a></li>
+                                                <li class="breadcrumb-item active" aria-current="page">Donation Form </li>
+                                            </ol>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- ============================================================== -->
+                        <!-- end pageheader  -->
+                        <!-- ============================================================== -->
+
+
+                        <div class="row">
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                <div class="section-block" id="basicform">
+                                    <h3 class="section-title">Enter Donation Data</h3>
+                                    
+                                </div>
+                                <div class="card">
+                                    
+                                    <div class="card-body">
+                                        <form>
+                                        <div class="form-group">
+                                                    <label for="inputText3" class="col-form-label">Today's Date</label>
+                                                <div class="input-group date" id="datetimepicker4" data-target-input="nearest">
+                                                    <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker4" />
+                                                    <div class="input-group-append" data-target="#datetimepicker4" data-toggle="datetimepicker" >
+                                                        <div class="input-group-text"><i class="far fa-calendar-alt"></i></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="inputText3" class="col-form-label">First Name</label>
+                                                <input id="inputText3" type="text" class="form-control">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="inputText3" class="col-form-label">Last Name</label>
+                                                <input  type="text" class="form-control">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="inputEmail">Email address</label>
+                                                <input id="inputEmail" type="email" placeholder="name@example.com" class="form-control">
+                                               
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="inputText4" class="col-form-label">Mobile Number</label>
+                                                <input id="inputText4" type="number" class="form-control" placeholder="Numbers">
+                                            </div>
+                                            <div class="form-group">
+                                                    <label for="inputText3" class="col-form-label">Address</label>
+                                                    <br>
+                                                    <label >Country</label>
+                                                    <select class="selectpicker dropup"  >
+                                                        
+                                                            <option>Egypt</option>
+                                                            <option>France</option>
+                                                            <option>America</option>
+                                                        </select>
+                                                        <br>
+                                                        <label for="inputText3" class="col-form-label">Gouvernement</label>
+
+                                                        <select class="selectpicker dropup">
+                                                                <option>Alex</option>
+                                                                <option>Giza</option>
+                                                                <option>Cairo</option>
+                                                            </select>
+                                                            <br>
+                                                            <label for="inputText3" class="col-form-label">City</label>
+                                                            <select class="selectpicker dropup">
+                                                                    <option>Shobra</option>
+                                                                    <option>Maadi</option>
+                                                                    <option>Nasr City</option>
+                                                                </select>
+                                                </div>
+                                                <div class="form-group">
+                                                <label for="inputText3" class="col-form-label">Donation type</label>
+                                                <br>
+                                                <label class="custom-control custom-radio custom-control-inline">
+                                                        <input type="radio" name="radio-inline" checked="" class="custom-control-input"><span class="custom-control-label">Cash</span>
+                                                    </label>
+                                                    <label class="custom-control custom-radio custom-control-inline">
+                                                        <input type="radio" name="radio-inline" class="custom-control-input"><span class="custom-control-label">Cheque</span>
+                                                    </label>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="inputText4" class="col-form-label">Amount of donation</label>
+                                                <input id="inputText4" type="number" class="form-control" placeholder="Numbers">
+                                            </div>
+                                            <div class="col-sm-6 pl-0">
+                                                <p class="text-right">
+                                                    <button type="submit" class="btn btn-space btn-primary">Submit</button>
+                                                    <button class="btn btn-space btn-secondary">Cancel</button>
+                                                </p>
+                                            </div>
+                                        </form>
+                                                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+                         <!-- footer -->
+            <!-- ============================================================== -->
+            <?php
+include '../dbfooter.html';
+
+?>
+            </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- end main wrapper -->
+    <!-- ============================================================== -->
+    <!-- Optional JavaScript -->
+    <script src="../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="../assets/vendor/slimscroll/jquery.slimscroll.js"></script>
+    <script src="../assets/libs/js/main-js.js"></script>
+    <script src="../assets/vendor/inputmask/js/jquery.inputmask.bundle.js"></script>
+    <script src="../assets/vendor/datepicker/moment.js"></script>
+    <script src="../assets/vendor/datepicker/tempusdominus-bootstrap-4.js"></script>
+    <script src="../assets/vendor/datepicker/datepicker.js"></script>
+
+</body>
+    </html>
