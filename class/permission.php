3f92a0db0d810b5559b205e7173a899a2dc2971a
yehyahamzawy <yehya1605639@miuegypt.edu.eg>
2019-03-17 20:13:33 +0200

    working on it

--- class/permission.php
+++ class/permission.php
@@ -2,7 +2,7 @@
     class permission 
     {
         private $id;
-        public $linkconnection;
+        public $linkconnection  = array();
         public $link = array();
 
         public function __construct($id)
@@ -11,16 +11,37 @@
         }
         function allData()
         {
-          
-            $Connection= new mysqli("localhost", "root", "", "se");
-            $sql = "SELECT * FROM link";
+            $this->link = array();
+            $this->linkconnection = array();
+            $Connection= new mysqli("localhost", "root", "", "olddb");
+            $sql = "SELECT * FROM links";
             $result = mysqli_query($Connection, $sql);
 
             while($Row = mysqli_fetch_array($result))
             array_push($this->link, $Row);
-            
-            
-      
+
+            $sql = "SELECT * FROM linkconnection";
+            $result = mysqli_query($Connection, $sql);
+
+            while($Row = mysqli_fetch_array($result))
+            array_push($this->linkconnection, $Row);
         }
+
+        function addPerm($linkID,$userTypeID)
+        {
+            $Connection= new mysqli("localhost", "root", "", "olddb");
+            $sql = "INSERT INTO linkconnection (userTypeID, linkID) VALUES (".$userTypeID.",".$linkID.")";
+        }
+
+        function returnUserTypeID($type)
+        {
+            $Connection= new mysqli("localhost", "root", "", "olddb");
+            $sql = "SELECT ID FROM usertype where type = '".$type."'";
+            //echo $sql;
+            $result = mysqli_query($Connection, $sql);
+            $typeID = mysqli_fetch_array($result);
+            return $typeID["ID"];
+        }
+
     }
 ?>
