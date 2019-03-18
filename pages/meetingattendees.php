5b970e08d2ddbc52b69c8c5702f7bd32af42f8a3
GoodGuyGarry <zuma222_hazem@hotmail.com>
2019-03-18 17:07:57 +0200

    finish

--- pages/meetingattendees.php
+++ pages/meetingattendees.php
@@ -54,14 +54,13 @@ include '../dashboard.html';
                         <div class="row">
                             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                                 <div class="page-header" id="top">
-                                    <h2 class="pageheader-title">Meeting Form </h2>
+                                    <h2 class="pageheader-title">Manage Meetings</h2>
                                     <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                                     <div class="page-breadcrumb">
                                         <nav aria-label="breadcrumb">
                                             <ol class="breadcrumb">
-                                            <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
-                                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Forms</a></li>
-                                                <li class="breadcrumb-item active" aria-current="page">Meeting Form</li>
+                                           
+                                                <li class="breadcrumb-item active" aria-current="page">Manage Meetings</li>
                                             </ol>
                                             </ol>
                                         </nav>
@@ -80,10 +79,7 @@ include '../dashboard.html';
                         <!-- ============================================================== -->
                         <div class="row">
                             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div class="section-block" id="basicform">
-                                    <h3 class="section-title">Enter Meeting Data</h3>
-                                    
-                                </div>
+                           
                                 <div class="card">
                                     
                                     <div class="card-body">
